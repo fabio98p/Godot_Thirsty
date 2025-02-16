b0bd0648441b91551c965ea47ff6b7a2cb730d77
fabio98p <parisifabio98@gmail.com>
2025-02-16 17:11:14 +0100

    fix hud and rename project name

--- HUD/hud.gd
+++ HUD/hud.gd
@@ -3,23 +3,15 @@ extends Control
 @onready var panel: Panel = $Panel
 @export var texture_path: Array[String]= []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("jump"):
-		print("ciao")
-		change_afterlife(1)
+	change_afterlife(4)
 
 #questo script si occuperà solo di una cosa, ovvero captare il segnale con un numero relativo alla vita del player, e 
 #in base al numero estratto cambierà la suddetta foto png schifosa della vita
 func change_afterlife(value: int):
-
-	change_panel_texture(load("res://Assets/life/life/onelife/h1.png"))
+	change_panel_texture(texture_path[value])
 	
-func change_panel_texture(new_texture: Texture):
-	panel.add_theme_stylebox_override("res://Assets/life/life/onelife/h1.png", StyleBoxTexture)
-	
+func change_panel_texture(new_texture: String):
+	var style2 : StyleBoxTexture = StyleBoxTexture.new()
+	style2.texture = load(new_texture)
+	panel.add_theme_stylebox_override("panel", style2)
