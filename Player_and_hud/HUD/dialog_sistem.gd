59ddb04cb083db9ee4091f72a8f9edf8a6ac8bbd
Zeta80 <axxel1700@gmail.com>
2025-02-16 23:05:27 +0100

    slow dialog text, button disabled

--- Player_and_hud/HUD/dialog_sistem.gd
+++ Player_and_hud/HUD/dialog_sistem.gd
@@ -5,6 +5,10 @@ extends Control
 @onready var ez_dialogue: EzDialogue = $EzDialogue
 @export var dialog_json: JSON 
 @onready var state= {}
+
+@export var text_speed = 0.01# Tempo tra una lettera e l'altra (in secondi)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ez_dialogue.start_dialogue(dialog_json, state)
@@ -17,7 +21,8 @@ func clear_dialog():
 	dialog.text = ""
 	
 func change_dialog(text: String):
-	dialog.text = text
+	dialogue_slow_effect(text)
+	#dialog.text = text
 
 func _on_ez_dialogue_dialogue_generated(response: DialogueResponse) -> void:
 	clear_dialog()
@@ -25,3 +30,16 @@ func _on_ez_dialogue_dialogue_generated(response: DialogueResponse) -> void:
 	
 func _on_button_pressed() -> void:
 	ez_dialogue.next()
+	
+	#funzioni per il testo in slowmotion mostra lettera per lettara disabilitando il bottone
+func dialogue_slow_effect(new_text: String):
+	button.disabled = true
+	var full_text = new_text
+	var current_text = ""
+	dialog.text = ""
+	for i in range(full_text.length()):
+		current_text += full_text[i]
+		dialog.text = current_text
+		await get_tree().create_timer(text_speed).timeout
+		if i == (full_text.length()-1):
+			button.disabled = false
