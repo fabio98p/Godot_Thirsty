64d711bc072b025fc37e7fead7a9110695a2337b
Zeta80 <axxel1700@gmail.com>
2025-02-17 03:56:15 +0100

    priorityze who is speaker imgtxt

--- Player_and_hud/HUD/dialog_sistem.gd
+++ Player_and_hud/HUD/dialog_sistem.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var player_img_dialogue: Panel = $Player_img_dialogue
+@onready var npc_img_dialogue: Panel = $Npc_img_dialogue
 @onready var speack_person: Label = $Speack_person
 @onready var dialog: Label = $VBoxContainer/Dialog
 @onready var button: Button = $VBoxContainer/Button
@@ -7,12 +9,12 @@ extends Control
 @export var dialog_json: JSON 
 @onready var state= {}
 
-@export var text_speed = 0.01# Tempo tra una lettera e l'altra (in secondi)
+@export var text_speed = 0.02# Tempo tra una lettera e l'altra (in secondi)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ez_dialogue.start_dialogue(dialog_json, state)
+	ez_dialogue.start_dialogue(dialog_json, state,)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,6 +24,8 @@ func clear_dialog():
 	dialog.text = ""
 	
 func change_dialog(text: String):
+	speack_person.text = state["dialogue_name"]
+	priorityze_speaker_imgtxt(state)
 	dialogue_slow_effect(text)
 	#dialog.text = text
 
@@ -49,5 +53,21 @@ func dialogue_slow_effect(new_text: String):
 func _on_ez_dialogue_custom_signal_received(value: Variant) -> void:
 	var params = value.split(",")
 	if params[0] == "set":
-		if params[1] == "dialogue_name":
-			speack_person.text = params[2]
+		var variable_name = params[1]
+		var variable_value = params[2]
+		state[variable_name] = variable_value
+		
+#vado a dare priotià di colore all immagine di chi sta parlando, tra player o npc
+func priorityze_speaker_imgtxt(state: Dictionary):
+	var player_dialogue_stylebox = player_img_dialogue.get_theme_stylebox("panel")
+	var npc_dialogue_stylebox = npc_img_dialogue.get_theme_stylebox("panel")
+	if state["dialogue_name"] == "Kevin":
+		
+		if player_dialogue_stylebox && npc_dialogue_stylebox is StyleBoxTexture :
+			player_dialogue_stylebox.modulate_color = Color(1,1,1,1) 
+			npc_dialogue_stylebox.modulate_color = Color(0.204, 0.204, 0.204, 0.686)
+			
+	else:
+		npc_dialogue_stylebox.modulate_color = Color(1,1,1,1) 
+		player_dialogue_stylebox.modulate_color = Color(0.204, 0.204, 0.204, 0.686)
+		
