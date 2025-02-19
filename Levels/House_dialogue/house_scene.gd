020ee8b4ff38e5f88bc764a95d144ef12e6c6ce6
Zeta80 <axxel1700@gmail.com>
2025-02-19 23:29:23 +0100

    npc dinamics dialogue

--- Levels/House_dialogue/house_scene.gd
+++ Levels/House_dialogue/house_scene.gd
@@ -7,6 +7,11 @@ extends Control
 	"bg_bath_room" : "",
 	"bg_entrance_house" : "",
 }
+@export var npc_expression:Dictionary = {
+	"idle" : "",
+	"sad" : "",
+	"speack" : "",
+}
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_node("/root/AudioStreamPlayer2d").playing = false
@@ -25,3 +30,10 @@ func change_background(value: Variant):
 		#state[variable_name] = variable_value
 		if variable_name == "background":
 			dialog_sistem.change_background_scene(backgrounds[variable_value])
+		if variable_name == "npc_name":
+			if variable_value == "null":
+				dialog_sistem.npc_img_dialogue.visible = false 
+			else:
+				dialog_sistem.npc_img_dialogue.visible = true 
+		if variable_name == "npc_expression":
+			dialog_sistem.change_npc_img_dialogue(npc_expression[variable_value])
--- Player_and_hud/HUD/dialog_sistem.gd
+++ Player_and_hud/HUD/dialog_sistem.gd
@@ -102,3 +102,8 @@ func change_pg_img_dialogue(img: String):
 	var style2 : StyleBoxTexture = StyleBoxTexture.new()
 	style2.texture = load(img)
 	player_img_dialogue.add_theme_stylebox_override("panel", style2)
+
+func change_npc_img_dialogue(img: String):
+	var style2 : StyleBoxTexture = StyleBoxTexture.new()
+	style2.texture = load(img)
+	npc_img_dialogue.add_theme_stylebox_override("panel", style2)
