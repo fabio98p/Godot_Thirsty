1ebe9bce1926a1ca8a883a86149dd793f93c1f1c
Zeta80 <axxel1700@gmail.com>
2025-02-20 20:52:37 +0100

    animation player

--- Player_and_hud/Player/player.gd
+++ Player_and_hud/Player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var inv_frame_timer: Timer = $InvFrameTimer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hud: Control = $"../HUD"
 var speed_slow = 100.0
 const SPEED = 300.0
@@ -7,7 +9,6 @@ const JUMP_VELOCITY = -400.0
 @export var hp_max := 4
 var current_hp = hp_max
 var isInvincible := false
-@onready var inv_frame_timer: Timer = $InvFrameTimer
 signal player_dead
 
 func _physics_process(delta: float) -> void:
@@ -15,14 +16,31 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("left", "right", "forward", "back")
 	if direction:
-		if Input.is_action_pressed("jump"):
+		if Input.is_action_pressed("slow_mode"):
 			self.velocity = direction * speed_slow
 		else:
 			self.velocity = direction * SPEED
 	else:
 		self.velocity = Vector2.ZERO
-		
+	change_animation(direction)	
 	move_and_slide()
+	
+func change_animation(direction: Vector2):
+	var animation: String
+	if direction.x == 0 && direction.y == 0:
+		animation= "idle"
+	elif direction.x >= 0.1 :
+		animation = "s_run"
+		animated_sprite_2d.flip_h = true
+	elif direction.x <= -0.1 :
+		animation = "s_run"
+		animated_sprite_2d.flip_h = false
+	elif direction.x == 0 && direction.y >= 0.1:
+		animation = "d_run"
+	elif direction.x == 0 && direction.y <= -0.1:
+		animation = "u_run"
+		
+	animated_sprite_2d.animation = animation
 	
 func lose_hp():
 	if !isInvincible:
